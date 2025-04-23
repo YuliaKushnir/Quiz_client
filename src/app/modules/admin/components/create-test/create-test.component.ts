859398397a1d5496398c2b729b2c7d703966aa03
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-23 08:21:24 +0300

    Added create test functionality

---
+++ src/app/modules/admin/components/create-test/create-test.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '../../services/admin.service';
+import { NzNotificationComponent, NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-test',
+  imports: [SharedModule],
+  templateUrl: './create-test.component.html',
+  styleUrl: './create-test.component.scss'
+})
+export class CreateTestComponent {
+
+  constructor(private fb: FormBuilder,
+    private devicesService: AdminService,
+    private notification: NzNotificationService,
+    private router: Router
+  ){}
+
+  testForm!: FormGroup;
+
+  ngOnInit(){
+    this.testForm = this.fb.group({
+      title: [null, Validators.required],
+      description: [null, [Validators.required]],
+      time: [null, [Validators.required]],
+
+    })
+  }
+
+  submitForm(){
+    if(this.testForm.valid){
+      this.devicesService.createTest(this.testForm.value).subscribe(res=>{
+        this.notification.success('SUCCESS', `Test created successfully`, {nzDuration:5000});
+        this.router.navigateByUrl("/admin/dashboard");
+      }, error => {
+        this.notification.error('ERROR', `${error.error}`, {nzDuration:5000});
+      })
+    }
+  }
+
+}
