50c02b036ff61f3c89a1410f6d749b3077db2920
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-25 08:11:33 +0300

    Create user service

--- src/app/modules/user/components/dashboard/dashboard.component.ts
+++ src/app/modules/user/components/dashboard/dashboard.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { TestService } from '../../services/test.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  tests =[];
+
+  constructor(private notification: NzNotificationService,
+    private testService: TestService){}
+
+    ngOnInit(){
+      this.getAllTests();
+    }
+  
+    getAllTests(){
+      this.testService.getAllTests().subscribe(res=>{
+        this.tests = res;
+      }, error => {
+        this.notification.error("ERROR", `Somethins went wrong`, {nzDuration: 5000});
+      })
+    }
+  
+    getFormattedTime(time): string{
+      const seconds = time % 60;
+      const minutes = Math.floor(time/60);
+      return `${minutes} minutes ${seconds} seconds`;
+    }
 
 }
