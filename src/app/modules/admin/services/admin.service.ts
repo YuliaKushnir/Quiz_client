105b2323dbb9d16aab450da90308e9702c70d1bd
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-26 13:38:00 +0300

    Added  functionality of test results for user

--- src/app/modules/admin/services/admin.service.ts
+++ src/app/modules/admin/services/admin.service.ts
@@ -28,4 +28,9 @@ export class AdminService {
     return this.http.get(BASIC_URL + `api/test/${id}`);
   }
 
+  
+  getTestResults(): Observable<any>{
+    return this.http.get(BASIC_URL + `api/test/test-result`);
+  }
+
 }
